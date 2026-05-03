b8394e67bf2e6708c384cd12ff43dd09c58bb3fa
mdfizz655 <info.mdfizz65@gmail.com>
2026-05-03 23:15:44 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // এটি বিল্ড করার সময় টাইপ এরর থাকলেও প্রোজেক্ট লাইভ করতে দিবে
+    
     ignoreBuildErrors: true,
   },
   eslint: {
-    // এটি ইএস-লিন্ট এরর ইগনোর করবে
+   
     ignoreDuringBuilds: true,
   },
 };
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    // এখানে কোনো ভুল থাকলে বিল্ড ফেইল হয়
+    
     baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
 });
