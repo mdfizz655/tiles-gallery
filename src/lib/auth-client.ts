22da68fca6061af5c289c430480085790ed623ef
mdfizz655 <info.mdfizz65@gmail.com>
2026-05-04 00:22:25 +0600

    update

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    
+    // এখানে কোনো স্লাশ ছাড়া সরাসরি ডোমেইন লিঙ্ক অথবা শুধু '/' দিন
     baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
 });
